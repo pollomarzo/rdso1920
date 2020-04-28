38ad0876497b09c468f1cb183f9b407e57b1978f
Paolo Marzolo <pollo.marzo@gmail.com>
2020-04-28 15:20:08 +0200

    working on checking airport

--- 2019exams/airport/airport.py
+++ 2019exams/airport/airport.py
@@ -10,6 +10,8 @@ BRX = 2
 LGW = 3
 FCO = 4
 AIRPORT_CODES = [BLQ, CDG, BRX, LGW, FCO]
+LENGTH = 3
+
 
 class airport(monitor):
     def __init__(self, MAX):
@@ -22,11 +24,13 @@ class airport(monitor):
         # dizionario: a un codice di stazione corrisponde una lista di bagagli
         self.cart_items = dict([(code, []) for code in AIRPORT_CODES])
         # dizionario: a un codice di stazione corrisponde una condition variable
-        self.waiting_to_unload = dict([(code, condition(self)) for code in AIRPORT_CODES])
+        self.waiting_to_unload = dict(
+            [(code, condition(self)) for code in AIRPORT_CODES])
 
     @entry
     def cartat(self, dstcode):
         self.station = dstcode
+        print(f"IN {AIRPORT_CODES[dstcode]}!")
 
         if self.n < self.MAX:
             print(f"Cart waiting for loading...")
@@ -44,7 +48,8 @@ class airport(monitor):
     def load(self, dstcode, owner):
         if self.n == self.MAX or self.station != BLQ:
             # aspetta che si liberi un posto e che il carrello torni a loading_station
-            print(f"Loading station {threading.current_thread().getName()} stopped")
+            print(
+                f"Loading station {threading.current_thread().getName()} stopped")
             self.waiting_to_load.wait()
         self.cart_items[dstcode]
         self.n += 1
@@ -64,18 +69,23 @@ class airport(monitor):
             self.waiting_to_unload[dstcode].wait()
         owner = self.cart_items[dstcode].pop(0)
         if self.cart_items[dstcode] != []:
-            print(f"Wake up other {dstcode} stations ({threading.current_thread().getName()})")
+            print(
+                f"Wake up other {dstcode} stations ({threading.current_thread().getName()})")
             self.waiting_to_unload[dstcode].signal()  # passing the bâton
         return owner
 
+
 def cart(mon):
-    while True:
+    for _ in range(LENGTH):
         for code in AIRPORT_CODES:
+            time.sleep(random.randint(0, 3))
             mon.cartat(code)    # il carrello è alla postazione code
+
 
 def loadingstation(mon):
     owner = 0
-    while True:
+    for _ in range(LENGTH):
+        time.sleep(random.randint(0, 3))
         dstcode = AIRPORT_CODES[random.randint(1, len(AIRPORT_CODES) - 1)]
         # carica la valigia del viaggiatore owner diretto a dstcode
         mon.load(dstcode, owner)
@@ -84,17 +94,21 @@ def loadingstation(mon):
 
 # C'è un processo stazione in ciascuna destinazione (tranne BLQ)
 def station(mon, dstcode):
-        while True:
-            # scarica dal carrello la valigia dell'utente owner
-            owner = mon.unload(dstcode)
-            print(f"Processing {owner} luggage to {dstcode}")
+    for _ in range(LENGTH):
+        time.sleep(random.randint(0, 3))
+        # scarica dal carrello la valigia dell'utente owner
+        owner = mon.unload(dstcode)
+        print(f"Processing {owner} luggage to {dstcode}")
+
 
 if __name__ == "__main__":
     order = ["l", "s"] * 5
     random.shuffle(order)
     print(f"Order: {order}")
     mon = airport(3)
-    threads = [threading.Thread(name="cart", target=cart, args=(mon,), daemon=True)]
+    threads = [threading.Thread(
+        name="cart", target=cart, args=(mon,), daemon=True)]
+    """
     nr = nw = 0
     for elem in order:
         if elem == "l":
@@ -104,12 +118,20 @@ if __name__ == "__main__":
         elif elem == "s":
             code = AIRPORT_CODES[random.randint(1, len(AIRPORT_CODES) - 1)]
             threads.append(threading.Thread(
-                name=f"station{nr}", target=station, args=(mon,code), daemon=True))
+                name=f"station{nr}", target=station, args=(mon, code), daemon=True))
             nr += 1
+    """
+    for airport in AIRPORT_CODES:
+        if airport is BLQ:
+            threads.append(threading.Thread(
+                name=f"loading_station{BLQ}", target=loadingstation, args=(mon,), daemon=True))
+        else:
+            code = AIRPORT_CODES[random.randint(1, len(AIRPORT_CODES) - 1)]
+            threads.append(threading.Thread(
+                name=f"station{airport}", target=station, args=(mon, code), daemon=True))
 
     for t in threads:
         t.start()
-        time.sleep(3)
 
     for t in threads:
         t.join()
